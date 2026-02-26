7c5d9109902e5c954d32a6f60735ca09f9b215fd
Marveen Riskwait <radiostation87.7.7@gmail.com>
2026-02-26 22:46:49 +0000

    First commit, card created, nothing interesting now

--- src/app.js
+++ src/app.js
@@ -5,7 +5,56 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function generateCard() {
+
+    const carta = document.querySelector("#carta")
+    const signs = ["♦", "♥", "♠", "♣"];
+    const numbersAndHeads = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+    function randomSign() {
+      let randomSign = signs[Math.floor(Math.random() * signs.length)]
+      if (randomSign === "♦" || randomSign === "♥") {
+        return `<span style="color: red;">${randomSign}</span>`;
+      };
+      return randomSign
+    }
+    let sign = randomSign()
+    function randomNumberAndHeads() {
+      let randomNumberAndHeads = numbersAndHeads[Math.floor(Math.random() * numbersAndHeads.length)]
+
+      return randomNumberAndHeads
+    }
+
+    carta.innerHTML = '<div class="content fs-1 container bg-light border rounded my-4 w-50 "></div>';
+    const content = document.querySelector(".content")
+    content.innerHTML = `<div>${sign}</div>` + `<div class="container py-5 text-center" style=" font-size:9rem" >${randomNumberAndHeads()}</div><div class="text-end">${sign}</div>`
+  }
+  generateCard()
+
+  let button = document.createElement("button");
+  button.innerHTML ="Click me!"
+  button.classList.add("btn-primary", "btn", "text-center")
+  document.body.appendChild(button)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
